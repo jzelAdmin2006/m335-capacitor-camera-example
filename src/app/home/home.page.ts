392a0ffc5d98891298a179862b33ea140654e670
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-01-10 10:01:34 +0100

    create camera example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { NgIf, NgOptimizedImage } from '@angular/common';
+
+interface Hero {
+  id: number;
+  name: string;
+  imgSrc?: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    NgIf,
+    NgOptimizedImage,
+  ],
 })
 export class HomePage {
+  hero: Hero = {
+    id: 2,
+    name: 'Windstorm',
+  };
+
   constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+    });
+    this.hero.imgSrc = image.webPath;
+  }
 }
